bf264da1ab50381e267f2564d87d2567439a5bc1
Akash singh <robinakash123@gmail.com>
2021-12-15 19:02:54 +0530

    PORT

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ app.post('/', (req, res) => {
     console.log(req.body)
 })
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
     console.log("Server is running on port 5000");
 })
